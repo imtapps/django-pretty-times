a68f8b3774c3940b9b77cde62dcd9784576543d3
Henry <toran.billups@theimtgroup.com>
2013-02-11 12:46:31 -0600

    updated readme and bumped version to 0.0.7

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ import re
 
 setup(
     name="django-pretty-times",
-    version='0.0.6',
+    version='0.0.7',
     author="imtapps",
     author_email="imtapps@apps-system.com",
     description="pretty_times provides django template helpers for the py-pretty library.",
-    long_description=open('README', 'r').read(),
+    long_description=open('README.rst', 'r').read(),
     url="http://github.com/imtapps/django-pretty-times",
     packages=find_packages(exclude=['example']),
     install_requires=file('requirements/base.txt').read().split("\n"),
